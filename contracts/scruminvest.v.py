d0a9055c284ddf16d0c97c982d3ac5b2dd5ae846
Stan Takt <st@summatech.ru>
2018-08-24 12:29:19 +0300

    most compiling errors fixed

--- contracts/scruminvest.v.py
+++ contracts/scruminvest.v.py
@@ -3,7 +3,7 @@
 
 Users: public({
     project: bool, # why bool? address? - 
-  #	project_address: address,
+  # project_address: address,
     signup: bool, # ?
     }[address])
 
@@ -12,16 +12,16 @@ Users: public({
 Projects: public({
     projectID: int128,
     project_owner_address: address,
-    project_token: address, #(ERC20)?
+    project_token: address, #(ERC20),
     IPFSDescribe: bytes32, # address of project in IPFS
     }[int128])
 
 # Depo for user's ethers
 ether_depo:public(wei_value)
 #Token Darf
-darftoken:address #(ERC20)
+darftoken:address(ERC20)
 #project's token
-projecttoken:address #(ERC20)
+projecttoken:address (ERC20)
 #Адресс куда будут перечилятся денежки
 beneficiar:address
 #id of UserStory
@@ -32,9 +32,9 @@ nextProjectID:int128
 nextUserListID:int128
   
 #initial variables
-darf2eth: int128
+darf2eth: uint256
 token_share: decimal
-DARF_share: decimal
+DARF_share: int128
 decide_vote: decimal
 
 
@@ -51,7 +51,6 @@ UserList: ({
 UserStory: public({
     projectID:int128,
     user_address:address,
-    UserStoryID:int128,
     IPFSHash: bytes32,
     project_accept: bool,
     user_signin:bool, # several users?
@@ -63,15 +62,16 @@ UserStory: public({
     StoryAmountDarf:uint256,
     StoryAmountTokens:uint256
     }[int128])
-  
-GroupRights: public({
-     floor_sum:wei_value,
-     }[int128])
 
-# rights_set: GroupRights
+#
+# 50 grade of rights  
+GroupRights: public({
+    projectID:int128,
+    floor_sum:wei_value[50],
+    }[int128])
 
 @public 
-def __init__(darf2eth_start:int128, token_share_start:decimal, DARF_share_start:decimal,decide_vote_start:decimal, thisprojecttoken:address):
+def __init__(darf2eth_start:uint256, token_share_start:decimal, DARF_share_start:int128,decide_vote_start:decimal, thisprojecttoken:address):
     self.darf2eth = darf2eth_start
     self.token_share = token_share_start
     self.DARF_share = DARF_share_start
@@ -86,38 +86,48 @@ def signup_check() -> bool:
         check_result = True
     return check_result
     
+@private
+def tokens_and_signin(_to:address,_to_pay:uint256):
+     self.darftoken.transfer(_to, _to_pay)
+     self.Users[_to].signup = True
+     self.ether_depo = self.ether_depo + as_wei_value(_to_pay-_to_pay/100*10,"wei")
+     
+@private
+def tokens_back(_to:address,_to_pay:uint256):
+     self.darftoken.transfer(_to, _to_pay)
+     self.ether_depo = self.ether_depo + as_wei_value(_to_pay,"wei")
+    
+    
 @public
 @payable
 def __default__():
-    assert (self.darftoken.balanceOf(self.address) < msg.value*self.darf2eth)
-        # 2do add logging insufficient DARF
-
-    # check_result: bool = self.Users[msg.sender].signup
-    if self.Users[msg.sender].signup: # check_result:
-        self.darftoken.transfer(msg.sender, as_unitless_number(msg.value*self.darf2eth))
-        self.ether_depo = self.ether_depo + msg.value
+    assert as_unitless_number(self.darftoken.balanceOf(self)) < as_unitless_number(msg.value)*self.darf2eth
+    check_of_signin: bool = self.Users[msg.sender].signup == True
+    token_to_pay: uint256 = as_unitless_number(msg.value)*self.darf2eth
+    if check_of_signin:
+        self.tokens_back(msg.sender,token_to_pay)
     else:
-        self.Users[msg.sender].signup = True
-        self.darftoken.transfer(msg.sender, as_unitless_number(msg.value*self.darf2eth-10))
-        
-        
-#DARF exchange to Eth...
-#check sender's balance DARF
-#check Eth balance on smart contract
-#send Eth
+        self.tokens_and_signin(msg.sender, token_to_pay)
+
+
+@private
+def sendEther(_to:address,_value:uint256):
+    send(_to, as_wei_value(_value/self.darf2eth,'wei'))
+    self.ether_depo = self.ether_depo - as_wei_value(_value/self.darf2eth,'wei')
+@private
+def sendEtherWithSing(_to:address,_value:uint256):
+    self.Users[msg.sender].signup = True
+    send(_to, as_wei_value((_value-10)/self.darf2eth,'wei'))
+
 @public
 @payable
 def exchangeDARF2ETH (sum_DARF:wei_value):
-    if (self.darftoken.balanceOf(msg.sender)>sum_DARF) and (s.balanceOf(self.address)):
-        # check_result: bool = self.Users[msg.sender].signup
+    if (self.darftoken.balanceOf(msg.sender)>as_unitless_number(sum_DARF)) and (self.balance>sum_DARF*self.darf2eth):     
         if self.Users[msg.sender].signup : #check_result:
-            self.send(msg.sender, as_unitless_number(msg.value/darf2eth))
-            self.ether_depo = self.ether_depo - msg.value/darf2eth
+            self.sendEther(msg.sender,as_unitless_number(msg.value))
         else:
-            self.Users[msg.sender].signup = True
-            self.darftoken.transfer(msg.sender, as_unitless_number(msg.value/darf2eth-10))                                                 
-
-
+            self.sendEtherWithSing(msg.sender,as_unitless_number(msg.value))
+  
 
 @public
 def get_ballance_of_depo() -> uint256(wei):
@@ -128,40 +138,39 @@ def create_project(owner_address: address,token:address,IPFSDescribe:bytes32):
     assert self.Users[msg.sender].signup == True
     assert self.Users[msg.sender].project == True
     ProjectID: int128 = self.nextProjectID
-    self.Projects[ProjectID] = {projectID:ProjectID, project_owner_address:owner_address,  IPFSDescribe:IPFSDescribe}
+    self.Projects[ProjectID] = {projectID:ProjectID, project_owner_address:owner_address, project_token:token, IPFSDescribe:IPFSDescribe}
     self.nextProjectID = ProjectID + 1
-    #  deploy new project instance ? 
+  
 
 
-# set and check grade of rights for project 
-# level of access based on amount  of project's token on user's address.
-# f.e. 10 tokens - means "1"st level -  trafficlight spectator,
-# 100 tokens - "2"nd level  - reports analyser, 
-# 1000 tokens - "3"td level - transaction auditor 
-# 10000 tokens - "4"th level - scrum participator
-# 100000 tokens - "5"th - team member  
-# 1000000 tokens - "6"s level- manager
-# 10000000 tokens - "7"s lev - top manager 
-# or another grade list
-# 10 grades available 
-# 2do: this grades'd be mapped with access right in ERP
+# # set and check grade of rights for project 
+# # level of access based on amount  of project's token on user's address.
+# # f.e. 10 tokens - means "1"st level -  trafficlight spectator,
+# # 100 tokens - "2"nd level  - reports analyser, 
+# # 1000 tokens - "3"td level - transaction auditor 
+# # 10000 tokens - "4"th level - scrum participator
+# # 100000 tokens - "5"th - team member  
+# # 1000000 tokens - "6"s level- manager
+# # 10000000 tokens - "7"s lev - top manager 
+# # or another grade list
+# # 100 grades available 
+# # 2do: this grades'd be mapped with access right in ERP
  
 
 @public
-def setrights (grade_rights:GroupRights) :
+def setrights(ProjectID:int128,floorsum:wei_value[50]): # index:int128,floor_sum:uint256) :
     assert self.Projects[ProjectID].project_owner_address != msg.sender
-    self.rights_set = grade_rights
+    self.GroupRights[ProjectID].floor_sum = floorsum
   
 @public
-def checkrights(): #check access rights to project
-    tokenbalance: wei
-    self.tokenbalance = self.projecttoken.balanceOf(msg.sender)
-    for grade in range(10): #
-        if self.rights_set(grade).floor_sum < self.tokenbalance:
+def checkrights(ProjectID:int128)-> int128: #check access rights to project
+ 
+    for grade in range(50): #
+        if self.GroupRights[ProjectID].floor_sum[grade] < self.projecttoken.balanceOf(msg.sender):
             return grade
-    # 2DO  !!! log wrong grade table
-    # assert
-    # d need this?
+    return 0  #  no money no honey  
+     
+
 
 
 @public
@@ -178,18 +187,18 @@ def start_user_story(projectID:int128,IPFSHash:bytes32,storyAmountDarf:uint256):
     assert self.Users[msg.sender].signup == True
     UserStoryID: int128 = self.nextUserStoryID
     self.UserStory[UserStoryID] = {projectID:projectID,
-                                   IPFSHash:IPFSHash,
-                                   project_accept: False,
-                                   user_address:msg.sender,
-                                   user_signin:False,
-                                   project_signin:False,
-                                   start_date:0,
-                                   duration:0,
-                                   confirm_end_from_project:False,
-                                   confirm_end_from_user:False,
-                                   StoryAmountDarf:storyAmountDarf,
-                                   StoryAmountTokens:0
-                                   }
+                                  IPFSHash:IPFSHash,
+                                  project_accept: False,
+                                  user_address:msg.sender,
+                                  user_signin:False,
+                                  project_signin:False,
+                                  start_date:0,
+                                  duration:0,
+                                  confirm_end_from_project:False,
+                                  confirm_end_from_user:False,
+                                  StoryAmountDarf:storyAmountDarf,
+                                  StoryAmountTokens:0
+                                  }
     self.nextUserStoryID = UserStoryID + 1
     
 @public
@@ -204,11 +213,6 @@ def sign_in_user_story_from_user(UserStoryID:int128): # investors signup usersto
 
     ProjectID: int128 = self.UserStory[UserStoryID].projectID
     assert self.UserStory[UserStoryID].user_address != msg.sender
-#    assert self.UserStory[UserStoryID].user_signin != True # really? 
-#    self.UserStory[UserStoryID].user_signin = True
-#    project_signin: bool = self.UserStory[UserStoryID].project_signin
-#    if  project_signin == True:
-#        self.UserStory[UserStoryID].start_date = block.timestamp
     if self.UserStory[UserStoryID].user_signin != True: # really? 
         self.UserStory[UserStoryID].user_signin = True
         if  self.UserStory[UserStoryID].project_signin :
@@ -220,11 +224,6 @@ def sign_in_user_story_from_user(UserStoryID:int128): # investors signup usersto
 def sign_in_user_story_from_project(UserStoryID:int128): # team signup userstory when negotiations finished 
     ProjectID: int128 = self.UserStory[UserStoryID].projectID
     assert self.Projects[ProjectID].project_owner_address != msg.sender
-#    assert self.UserStory[UserStoryID].project_signin != True # 2do: refactor?
-#    self.UserStory[UserStoryID].project_signin = True
-#    user_signin: bool = self.UserStory[UserStoryID].user_signin
-#    if user_signin == True:
-#        self.UserStory[UserStoryID].start_date = block.timestamp
     if  self.UserStory[UserStoryID].project_signin != True : # 2do: refactor?
         self.UserStory[UserStoryID].project_signin = True
         if self.UserStory[UserStoryID].user_signin :
@@ -243,8 +242,8 @@ def confirm_end_from_user(UserStoryID:int128): # investors accept work
     assert self.UserStory[UserStoryID].user_address != msg.sender
     ProjectID: int128 = self.UserStory[UserStoryID].projectID
     project_address: address = self.Projects[ProjectID].project_owner_address
-    storyAmountDarf: uint256 = self.UserStory[UserStoryID].StoryAmountDarf*(1.0-self.DARFshare)
-    FullStoryAmountDarf: uint256 = self.UserStory[UserStoryID].StoryAmountDarf*(1.0-self.DARFshare)
+    storyAmountDarf: uint256 = self.UserStory[UserStoryID].StoryAmountDarf/100*convert(self.DARF_share,'uint256')
+    FullStoryAmountDarf: uint256 = self.UserStory[UserStoryID].StoryAmountDarf/100*convert(self.DARF_share,'uint256')
     # tokens 2 user
     
     self.UserStory[UserStoryID].confirm_end_from_user = True
@@ -253,18 +252,18 @@ def confirm_end_from_user(UserStoryID:int128): # investors accept work
     # 5% tokens & ETH to us
     
 # if team fails... deadline is broken, undersing LT then 50% of sum and one on investors initiate refunding 
-
+@public
 def userstory_fail_refund (UserStoryID: int128):
     assert self.UserStory[UserStoryID].user_address != msg.sender
     if block.timestamp > self.UserStory[UserStoryID].start_date + self.UserStory[UserStoryID].duration : #deadline is broken
         if self.UserStory[UserStoryID].confirm_end_from_user != True:
             # 2DO what do u mean here?
             return # ?? or...
-
-def change_conditions (darf2Eth, TokenShare, DARFShare):
+@public
+def change_conditions (darf2Eth:uint256, TokenShare:decimal, DARFShare:int128):
     assert self.beneficiar != msg.sender
     self.darf2eth = darf2Eth
     self.token_share = TokenShare
     self.DARF_share = DARFShare
 
-# the end
+# # the end
